cd2956eceeeb48239cf412343ebdd80c4e7db2af
Lawrence <Caulker>
2020-07-05 01:43:38 -0400

    L112: created Controlled Input

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,8 +1,36 @@
 import React, { Component } from "react"
 
 export class SearchBar extends Component {
+  state = { term: "" }
+
+  onInputChange = event => {
+    this.setState({
+      term: event.target.value,
+    })
+  }
+
+  onFormSubmit = event => {
+    event.preventDefault()
+
+    // Todo: Add callback from parent component
+  }
+
   render() {
-    return <div>SearchBar</div>
+    return (
+      <div className="search-bar ui segment">
+        <form onSubmit={this.onFormSubmit} className="ui form">
+          <div className="field">
+            <label>Video Search</label>
+            <input
+              type="text"
+              placeholder="Enter search term"
+              value={this.state.term}
+              onChange={this.onInputChange}
+            />
+          </div>
+        </form>
+      </div>
+    )
   }
 }
 
